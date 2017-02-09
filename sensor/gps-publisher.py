89a03019e1e1eecb78b46b0534e6b2793c0178d0
Olivier Koch <olivier@olivierkoch.org>
2017-02-09 15:16:31 +0000

    new gps publisher

--- sensor/gps-publisher.py
+++ sensor/gps-publisher.py
@@ -11,27 +11,77 @@ from exlcm import gps_t
 gpsd = None #seting the global variable
  
 class GpsPoller(threading.Thread):
-  def __init__(self):
-    threading.Thread.__init__(self)
-    global gpsd #bring it in scope
-    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-    self.current_value = None
-    self.running = True #setting the thread running to true
+    """ class for GPS poller """
+    def __init__(self):
+        threading.Thread.__init__(self)
+        global gpsd #bring it in scope
+        gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+        self.current_value = None
+        self.running = True #setting the thread running to true
  
-  def run(self):
-    global gpsd
-    while gpsp.running:
-      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+    def run(self):
+        global gpsd
+        while gpsp.running:
+            gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
-def send_gps_signal (lc, lat, lon):
 
+def read_gps_data_from_file (filename):
+    """ read GPS data from file """
+    try:
+        with open (filename,'r') as fp:
+            d = [float(a) for a in fp.readline().split(' ')]
+            return (d[0],d[1],int(d[2]), True)
+    except:
+        return (.0, .0, 0, False)
+
+
+def write_gps_data_to_file (filename, lat, lon, gps_timestamp):
+    """ write GPS data to file """
+    with open (filename, 'w') as fp:
+        fp.write ('%.5f %.5f %d' % (lat, lon, gps_timestamp))
+
+
+def is_valid_data (lat, lon):
+    """ test for GPS data validity """
+    if isnan(lat) or isnan(lon):
+        return False
+    if lat < 1E-6 or lon < 1E-6:
+        return False
+    return True
+
+
+def publish_msg (lat, lon, gps_timestamp, enabled):
+    """ publish GPS message """
     msg = gps_t()
-    msg.timestamp = int(time.time() * 1000000)
+    msg.timestamp = int(time.time())
     msg.lat = lat
     msg.lon = lon
-    msg.enabled = True
-    print ('gps-sim: published message at %d. Lat = %.5f, Lon = %.5f' % (msg.timestamp, msg.lat, msg.lon))
+    msg.enabled = enabled
+    msg.gps_timestamp = gps_timestamp
     lc.publish("GPS", msg.encode())
+
+
+def send_gps_signal (lc, lat, lon):
+    """ send LCM signal with GPS data """
+    gps_data_filename = '/home/pi/gps_data.txt'
+    time_now = int(time.time())
+    if is_valid_data (lat, lon):
+        # gps data is valid, publish it and save it to file
+        gps_timestamp = time_now
+        publish_msg (lat, lon, gps_timestamp, True)
+        write_gps_data_to_file (gps_data_filename, lat, lon, gps_timestamp)
+        print ("[gps] GPS OK.  lat = %.5f, lon = %.5f" % (lat, lon))
+    else:
+        # try to read from file
+        (lat, lon, gps_timestamp, is_valid) = read_gps_data_from_file (gps_data_filename)
+        if is_valid:
+            # gps data is invalid, read it from file
+            publish_msg (lat, lon, gps_timestamp, False)
+            delta_hours = (time_now - gps_timestamp)*1.0 / 3600
+            print ("[gps] GPS DOWN.  Fallback values lat = %.5f, lon = %.5f (%.2f hours late)" % (lat, lon, delta_hours))
+        else:
+            print ("[gps] ERROR No GPS data available.  Failed to read backup file %s.  Skipping..." % gps_data_filename)
+            
 
 
 if __name__ == '__main__':
